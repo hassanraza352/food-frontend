14523d0b0ffb900224fe77add04080e30405eba8
Muhammad Hassan Raza <mhassanr715@gmail.com>
2026-07-22 16:07:51 +0500

    ab

--- vite frontend/src/VerifyOtp.jsx
+++ vite frontend/src/VerifyOtp.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./css/otp.css"
+import "./css/OTP.css"
 
 function VerifyOtp() {
 
